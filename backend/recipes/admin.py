48ad8513c442f73beb7f4fbcb8d06849e38e71af
Terminal3D <66626023+Terminal3D@users.noreply.github.com>
2024-04-08 22:31:31 +0300

    updated models

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, Ingredient, Recipe, Favorite, ShoppingCart, Subscription, RecipeIngredient
+
+
+class RecipeIngredientsInLine(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+
+
+class RecipeIngredientsAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientsInLine]
+
+
+admin.site.register(Tag)
+admin.site.register(Ingredient)
+admin.site.register(Recipe, RecipeIngredientsAdmin)
+admin.site.register(RecipeIngredient)
+admin.site.register(Favorite)
+admin.site.register(ShoppingCart)
+admin.site.register(Subscription)
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -7,35 +7,62 @@ class Tag(models.Model):
     color = models.CharField(max_length=7, unique=True)
     slug = models.SlugField(unique=True)
 
+    def __str__(self):
+        return self.slug
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=256)
     measurement_unit = models.CharField(max_length=256)
 
+    def __str__(self):
+        return self.name
 
-class IngredientInstance(models.Model):
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    amount = models.IntegerField()
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE, related_name='recipe_ingredients')
+    ingredient = models.ForeignKey('Ingredient', on_delete=models.CASCADE, related_name='ingredient_recipes')
+    amount = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "Recipe: " + self.recipe.__str__() + " Ingredient: " + self.ingredient.__str__()
+
+    class Meta:
+        unique_together = ('recipe', 'ingredient')
 
 
 class Recipe(models.Model):
-    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='user_recipes')
     title = models.CharField(max_length=256)
-    tags = models.ManyToManyField(Tag, related_name='recipes')
-
-
-class RecipeInstance(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.PROTECT)
-    description = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name='tagged_recipes', blank=True)
+    ingredients = models.ManyToManyField(Ingredient, through=RecipeIngredient, related_name='recipes')
+    description = models.TextField(default='', blank=True)
     image = models.ImageField(upload_to='recipe_images', default='recipe_images/default.jpg')
-    cooking_time = models.IntegerField()
+    cooking_time = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
 
 
 class Favorite(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorites')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='favorite_by')
+
+    def __str__(self):
+        return self.recipe.__str__() + " by " + self.user.__str__()
 
 
 class ShoppingCart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='shopping_cart')
+    recipes = models.ManyToManyField(Recipe, related_name='in_shopping_carts')
+
+    def __str__(self):
+        return self.user
+
+
+class Subscription(models.Model):
+    subscriber = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+
+    def __str__(self):
+        return self.author.__str__() + " followed by" + self.subscriber.__str__()
